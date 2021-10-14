4a7a3241d1723311236f4dde23c70bcc5c6f1f7b
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-14 18:20:38 +0300

    Update itc_count_char_in_str.cpp

--- itc_count_char_in_str.cpp
+++ itc_count_char_in_str.cpp
@@ -1,8 +1,8 @@
 #include "str_easy.h"
 
 int itc_count_char_in_str(char ch, string str){
-    int res;
-    for ( int i = 0, i < itc_len(str), i++){
+    int res = 0;
+    for ( int i = 0; i < itc_len(str); i++){
         if (str[i] == ch)
             res++;
     }
