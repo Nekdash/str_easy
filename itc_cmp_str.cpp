bb6f4d88257899aa5ea5a88d80d9836ecd13d9ea
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-14 18:01:57 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -1,6 +1,13 @@
 #include "str_easy.h"
 
 string itc_cmp_str(string str1, string str2, int num){
-
+    string res = "";
+    for ( int i = 0; i < num;i++){
+        res += str[i];
+    }
+  for ( int i = num; i< itc_len(str);i++){
+      res += str[i];
+  }
+  return res;
 
 }
