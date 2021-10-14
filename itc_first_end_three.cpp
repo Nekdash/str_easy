d16d0c25d9a8c90cc7b990b8fdd1389e76e1670f
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-14 18:20:46 +0300

    Update itc_first_end_three.cpp

--- itc_first_end_three.cpp
+++ itc_first_end_three.cpp
@@ -10,14 +10,14 @@
 */
 void itc_first_end_three(string str){
     if (itc_len(str) > 5){
-        for (int i =0 , i< 3, i++){
+        for (int i =0 ; i< 3; i++){
             cout << str[i];
         }
-        for (int k = 0, k < 3, k++){
-            cout << str[itc_len(str)-1-k];
+        for (int k = 3; k > 0; k--){
+            cout << str[itc_len(str)-k];
         }
     }else{
-        for (int j = 0, j < itc_len(str), j++){
+        for (int j = 0; j < itc_len(str); j++){
             cout << str[j];
         }
     }
