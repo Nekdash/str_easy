c86ae94d47b64f602d5436ff795d52d8fc0ce346
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-15 09:30:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main()
 {
-    itc_first_end_three("abcththtabcx");
+    string str = "nekrasova";
+    cout << itc_hello_str(str) << endl;
+    cout << itc_len(str) << endl;
+    itc_print_copy_str(str, 3);
+    itc_first_end_three(str);
+    cout << endl;
+    cout << itc_count_char_in_str('a', str) << endl;
+    cout << itc_even_place(str) << endl;
+    cout << itc_percent_lower_uppercase(str) << endl;
+    cout << itc_reverse_str(str) << endl;
+    cout << itc_slice_str(str, 1, 5) << endl;
+    cout << itc_equal_reverse(str) << endl;
+    cout << itc_cmp_str(str, "dash", 4) << endl;
     return 0;
 }
