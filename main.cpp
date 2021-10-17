c83e035305127a032d6e70705860ae4553a4b82b
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-17 16:26:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main(){
 
-
-
+    string str1, str2, str3;
+    getline(cin, str1);
+    getline(cin, str2);
+    getline(cin, str3);
+    cout << itc_three_str(str1, str2, str3) << endl;
     return 0;
     }
