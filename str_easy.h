52bee4166694ca2d273ed195c6c858127f0b1b3a
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-17 16:26:10 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -7,8 +7,8 @@ using namespace std;
 
 string itc_hello_str(string name);
 long long itc_len(string str);
-string itc_print_copy_str(string str, int number);
-string itc_first_end_three(string str);
+void itc_print_copy_str(string str, int number);
+void itc_first_end_three(string str);
 int itc_count_char_in_str(char ch, string str);
 string itc_even_place(string str);
 double itc_percent_lower_uppercase(string str);
