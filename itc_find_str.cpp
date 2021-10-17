9d3813854c2269d55c3896cd5c23a40b4430eac6
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-17 16:00:05 +0300

    Update itc_find_str.cpp

--- itc_find_str.cpp
+++ itc_find_str.cpp
@@ -5,7 +5,7 @@ long long itc_find_str(string str1, string str2){
     bool check = true;
     long long res = -1;
     for ( int i = 0; i < itc_len(str1); i++){
-        if (itc_slice_str(str1, i, i+itc_len(str2)-1) == str2){
+        if (itc_slice_str(str1, i, i+itc_len(str2)) == str2){
             res = i;
             break;
         }
