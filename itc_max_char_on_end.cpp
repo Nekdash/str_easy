02c8548db0a8e7ca51c761d523f0fe6772720955
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-17 16:47:47 +0300

    Update itc_max_char_on_end.cpp

--- itc_max_char_on_end.cpp
+++ itc_max_char_on_end.cpp
@@ -1,4 +1,20 @@
 #include "str_easy.h"
 
  long long itc_max_char_on_end(string str){
+     int ch;
+     long long res = 0;
+     long long counter =0;
+     for ( int i = 0; i < itc_len(str); i++){
+        ch = str[i];
+        if ( ch >= 48 && ch <= 57){
+            counter += 1;
+        }
+        else{
+            if (counter > res){
+                res = counter;
+            }
+            counter = 0;
+        }
+     }
+     return res;
  }
