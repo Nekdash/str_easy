606ef6eeb19e94cccd4c9000eda32b79d547e8e5
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-18 12:42:16 +0300

    Update itc_three_str.cpp

--- itc_three_str.cpp
+++ itc_three_str.cpp
@@ -4,7 +4,7 @@ string itc_three_str(string str1, string str2, string str3){
     long long i =0;
     while (itc_find_str(itc_slice_str(str1, i , itc_len(str1)), str2) != -1){
             long long start = itc_find_str(itc_slice_str(str1, i , itc_len(str1)), str2);
-            str1 = itc_slice_str(str1, 0, start) + str3 + itc_slice_str(str1, start + itc_len(str2), itc_len(str1));
+            str1 = itc_slice_str(str1, 0, start-1) + str3 + itc_slice_str(str1, start + itc_len(str2), itc_len(str1)-1);
             i = start + itc_len(str3) - 1;
         }
         return str1;
