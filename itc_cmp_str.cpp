5c945a9cb487d4dafbe3553af87b5bf00a55fc0a
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-08 11:06:33 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -5,7 +5,7 @@ string itc_cmp_str(string str1, string str2, int num){
     for ( int i = 0; i < num;i++){
         res += str1[i];
     }
-    res += itc_slice_str(str2, 0, (len1 - num));
+    res += itc_slice_str(str2, 0, (itc_len(str1) - num));
   for ( int j = num-1; j< itc_len(str1);j++){
       res += str1[j];
   }
