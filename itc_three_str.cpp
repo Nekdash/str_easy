50e106f3c470bfe7e466f40d5d525837fbbd655b
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-11 11:19:03 +0300

    Update itc_three_str.cpp

--- itc_three_str.cpp
+++ itc_three_str.cpp
@@ -7,7 +7,7 @@ string itc_three_str(string str1, string str2, string str3){
     len = itc_len(str2);
     while (itc_find_str(res, str2) != -1){
             str1= res;
-            i = itc_find(str1, str2);
+            i = itc_find_str(str1, str2);
             res = "";
             for ( int j = 0; j < i; j++){
                 res += str1;
