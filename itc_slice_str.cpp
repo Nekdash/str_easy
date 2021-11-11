a5392db3dbe2041be94d68dd97e68448a8e7bb02
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-11 12:03:03 +0300

    Update itc_slice_str.cpp

--- itc_slice_str.cpp
+++ itc_slice_str.cpp
@@ -3,8 +3,9 @@
 string itc_slice_str(string str, int start, int endd){
     string res ="";
     if ( endd >= itc_len(str)){
-        endd = itc_len(str);
-    }else if(start > endd){
+        endd = itc_len(str) - 1;
+    }
+    if(start > endd){
         return str;
     }else if(start == (itc_len(str) - 1)){
         res += str[itc_len(str) - 1];
